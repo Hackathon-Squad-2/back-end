fb05cb232ad5564bbc905d1f8d19916bdbeebc80
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-01 03:13:13 -0300

    feat(express): express setup and hello world route

--- src/index.spec.ts
+++
@@ -1,7 +0,0 @@
-describe('Testing', () => {
-  it('should return true if 2 + 2 equal 4', () => {
-    const value = 2 + 2;
-
-    expect(value).toEqual(4);
-  });
-});
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,10 @@
-const message = 'Hello, World!';
+import express from 'express';
+import { Request, Response } from 'express';
+
+const app = express();
+
+app.get('/', (_request: Request, response: Response) => {
+  return response.status(200).json({ message: 'Hello, World!' });
+});
+
+app.listen(3000, () => console.log('Server is running on port 3000!'));
