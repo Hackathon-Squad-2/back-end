4372bc59cd96ea98fcade48b83d90e4ffdbe4aaa
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-01 14:56:37 -0300

    docs(swagger): swagger configuration and api.schema
    also added the first route documentation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,18 @@
 import 'dotenv/config';
 import express from 'express';
 import { Request, Response } from 'express';
+import swaggerUI from 'swagger-ui-express';
 
 import { ConnectDB, prisma } from './database';
+
+import apiSchema from '../docs/api.schema.json';
 
 const app = express();
 app.use(express.json());
 
 ConnectDB();
+
+app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(apiSchema));
 
 app.get('/', (_request: Request, response: Response) => {
   return response.status(200).json({ message: 'Hello, World!' });
