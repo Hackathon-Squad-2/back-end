d7292b1146b52afd074508847253cb863f6de631
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-04 19:22:59 -0300

    refactor(express): errorHandler middleware for express

--- src/shared/infra/http/app.ts
+++ src/shared/infra/http/app.ts
@@ -2,36 +2,27 @@ import 'dotenv/config';
 import 'express-async-errors';
 
 import cors from 'cors';
-import express, { NextFunction, Request, Response } from 'express';
+import express from 'express';
 import swaggerUI from 'swagger-ui-express';
 
-import { AppError } from '../../errors/AppError';
 import { ConnectDB } from '../../../database';
 
 import { errors } from 'celebrate';
 import { router } from './routes';
 
 import apiSchema from '../../../../docs/api.schema.json';
+import { errorHandler } from './middlewares/errorHandler';
 
 export const app = express();
 
 ConnectDB().then(() => {
   app.use(cors());
+
   app.use(express.json());
   app.use('/api/docs', swaggerUI.serve, swaggerUI.setup(apiSchema));
 
   app.use(router);
   app.use(errors());
 
-  app.use(
-    (err: Error, _request: Request, response: Response, next: NextFunction) => {
-      if (err instanceof AppError) {
-        return response.status(err.statusCode).json({
-          message: err.message,
-        });
-      }
-
-      next(err);
-    }
-  );
+  app.use(errorHandler);
 });
