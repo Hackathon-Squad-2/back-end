1836ace618549dd8e1b66af25921c49a055c4f6b
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-04 18:58:29 -0300

    feat(express): user profile route
    also tried to change a eslint config to fix a bug and couldn't

--- src/modules/users/useCases/userProfile/UserProfileUseCase.ts
+++ src/modules/users/useCases/userProfile/UserProfileUseCase.ts
@@ -1,16 +1,24 @@
-import { User } from '@prisma/client';
-
 import { prisma } from '../../../../database';
 import { AppError } from '../../../../shared/errors/AppError';
 
+type IResponse = {
+  name: string;
+  email: string;
+  createAt: Date;
+};
+
 export class UserProfileUseCase {
-  async execute(id: string): Promise<User> {
+  async execute(id: string): Promise<IResponse> {
     const user = await prisma.user.findFirst({ where: { id } });
 
     if (!user) {
       throw new AppError('Unauthorized', 401);
     }
 
-    return user;
+    return {
+      name: user.name,
+      email: user.email,
+      createAt: user.createdAt,
+    };
   }
 }
