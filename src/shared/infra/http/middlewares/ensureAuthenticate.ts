122543b91ae4ac0b212de44c81fd2e0e36e776ff
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-04 15:40:17 -0300

    feat(express): added ensureAuthenticate middleware

---
+++ src/shared/infra/http/middlewares/ensureAuthenticate.ts
@@ -0,0 +1,32 @@
+import { verify } from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+
+import { AppError } from '../../../errors/AppError';
+
+interface IPayload {
+  sub: string;
+}
+
+export const ensureAuthenticated = async (
+  request: Request,
+  _response: Response,
+  next: NextFunction
+) => {
+  const authHeader = request.headers.authorization;
+
+  if (!authHeader) {
+    throw new AppError('Token missing', 401);
+  }
+
+  const [, token] = authHeader.split(' ');
+
+  try {
+    const { id } = verify(token, process.env.JWT_PASS ?? '') as IPayload;
+
+    request.user = { id };
+
+    next();
+  } catch (error) {
+    throw new AppError('Invalid token', 401);
+  }
+};
