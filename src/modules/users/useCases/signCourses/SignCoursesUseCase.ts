14481b015529b0da4fe5bdf08d679ce2f9c396da
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-04 21:52:56 -0300

    feat: finished sign courses implementation
    also added a new way to handle errors from prisma

--- src/modules/users/useCases/signCourses/SignCoursesUseCase.ts
+++ src/modules/users/useCases/signCourses/SignCoursesUseCase.ts
@@ -1,6 +1,8 @@
 import { Prisma } from '@prisma/client';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
 
 import { prisma } from '../../../../database';
+import { AppError } from '../../../../shared/errors/AppError';
 
 type IRequest = {
   id: string;
@@ -15,7 +17,20 @@ export class SignCoursesUseCase {
         skipDuplicates: true,
       });
     } catch (err) {
-      console.log(err);
+      if (err instanceof PrismaClientKnownRequestError) {
+        const field = err.meta?.field_name as string | undefined;
+
+        if (field) {
+          if (field.includes('trailId')) {
+            throw new AppError('Course not found', 404);
+          }
+
+          if (field.includes('userId')) {
+            throw new AppError('Unauthorized', 401);
+          }
+        }
+      }
+
       throw err;
     }
   }
