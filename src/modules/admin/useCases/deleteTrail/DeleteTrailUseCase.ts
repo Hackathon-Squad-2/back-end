96202b8d5752ebc53a2e91ee996afa966c32b867
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-06 20:35:26 -0300

    refactor(admin): changed how the delete trail works
    also added the deletedAt field to trail and content on the db also refactores the delete trail

--- src/modules/admin/useCases/deleteTrail/DeleteTrailUseCase.ts
+++ src/modules/admin/useCases/deleteTrail/DeleteTrailUseCase.ts
@@ -3,12 +3,20 @@ import { AppError } from '../../../../shared/errors/AppError';
 
 export class DeleteTrailUseCase {
   async execute(id: string) {
-    const trail = await prisma.trail.findFirst({ where: { id } });
+    const trail = await prisma.trail.findFirst({
+      where: { id, deletedAt: null },
+    });
 
     if (!trail) {
       throw new AppError('Trail not found', 404);
     }
 
-    return await prisma.trail.delete({ where: { id } });
+    return await prisma.trail.update({
+      where: { id },
+      data: {
+        ...trail,
+        deletedAt: new Date(),
+      },
+    });
   }
 }
