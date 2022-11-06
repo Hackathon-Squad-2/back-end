e02a13ec1b138b274b681afcd96aa30be65afe64
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-05 21:14:40 -0300

    feat(express): added the edit trail route

--- src/shared/infra/http/routes/admin.routes.ts
+++ src/shared/infra/http/routes/admin.routes.ts
@@ -5,12 +5,18 @@ import {
   createTrailValidation,
 } from '../../../../modules/admin/useCases/createTrail';
 
+import {
+  EditTrailController,
+  editTrailValidation,
+} from '../../../../modules/admin/useCases/editTrail';
+
 import { ensureAdmin } from '../middlewares/ensureAdmin';
 import { ensureAuthenticated } from '../middlewares/ensureAuthenticate';
 
 export const adminRoutes = Router();
 
 const createTrailController = new CreateTrailController();
+const editTrailController = new EditTrailController();
 
 adminRoutes.post(
   '/trails',
@@ -18,4 +24,12 @@ adminRoutes.post(
   ensureAdmin,
   createTrailValidation,
   createTrailController.handle
+);
+
+adminRoutes.patch(
+  '/trails/:trailId',
+  ensureAuthenticated,
+  ensureAdmin,
+  editTrailValidation,
+  editTrailController.handle
 );
