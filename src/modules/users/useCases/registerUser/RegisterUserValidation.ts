f32017d3d35e8ee8fbe58359d2cd4adfd0b74a88
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-09 18:41:24 -0300

    refactor(users): removed the min length requiremento for the name
    for now

--- src/modules/users/useCases/registerUser/RegisterUserValidation.ts
+++ src/modules/users/useCases/registerUser/RegisterUserValidation.ts
@@ -2,7 +2,7 @@ import { celebrate, Joi, Segments } from 'celebrate';
 
 export const registerUserValidation = celebrate({
   [Segments.BODY]: {
-    name: Joi.string().min(4).required(),
+    name: Joi.string().required(),
     email: Joi.string().email().required(),
     password: Joi.string().required(),
   },
