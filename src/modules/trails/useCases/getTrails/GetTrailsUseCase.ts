df7be5b5c5bbd795a112cfc554208520f4967fa7
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 12:28:41 -0300

    refactor(trails): added the id to the return info

--- src/modules/trails/useCases/getTrails/GetTrailsUseCase.ts
+++ src/modules/trails/useCases/getTrails/GetTrailsUseCase.ts
@@ -4,6 +4,7 @@ export class GetTrailsUseCase {
   async execute() {
     return await prisma.trail.findMany({
       select: {
+        id: true,
         title: true,
         description: true,
         creator: true,
