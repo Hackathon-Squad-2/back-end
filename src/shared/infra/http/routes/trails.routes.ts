2c051b7c42c6afdb34963a8ae466b5584950a4f3
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-14 17:58:19 -0300

    feat(express): added trail routes implementation

--- src/shared/infra/http/routes/trails.routes.ts
+++ src/shared/infra/http/routes/trails.routes.ts
@@ -1,9 +1,13 @@
 import { Router } from 'express';
+import { GetTrailContentsController } from '../../../../modules/trails/useCases/getTrailContents';
 
 import { GetTrailsController } from '../../../../modules/trails/useCases/getTrails';
 
 export const trailsRoutes = Router();
 
 const getTrailsController = new GetTrailsController();
+const getTrailContentsController = new GetTrailContentsController();
 
 trailsRoutes.get('/', getTrailsController.handle);
+
+trailsRoutes.get('/:trailId/content', getTrailContentsController.handle);
