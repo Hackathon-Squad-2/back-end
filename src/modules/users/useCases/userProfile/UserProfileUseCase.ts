3636218b250a42fcf2af99f92d73a81ce4a614cb
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-15 10:51:35 -0300

    refactor(users): changed the get user profile return

--- src/modules/users/useCases/userProfile/UserProfileUseCase.ts
+++ src/modules/users/useCases/userProfile/UserProfileUseCase.ts
@@ -1,24 +1,38 @@
 import { prisma } from '../../../../database';
 import { AppError } from '../../../../shared/errors/AppError';
 
-type IResponse = {
-  name: string;
-  email: string;
-  createAt: Date;
-};
-
 export class UserProfileUseCase {
-  async execute(id: string): Promise<IResponse> {
+  async execute(id: string) {
     const user = await prisma.user.findFirst({ where: { id } });
 
     if (!user) {
       throw new AppError('Unauthorized', 401);
     }
 
-    return {
-      name: user.name,
-      email: user.email,
-      createAt: user.createdAt,
-    };
+    const courses = await prisma.course.findMany({
+      where: { userId: id },
+    });
+
+    return await prisma.trail.findMany({
+      where: {
+        id: {
+          in: courses.map((course) => course.trailId),
+        },
+        deletedAt: null,
+      },
+      select: {
+        id: true,
+        title: true,
+        content: {
+          select: {
+            progress: {
+              select: {
+                status: true,
+              },
+            },
+          },
+        },
+      },
+    });
   }
 }
