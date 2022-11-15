8e352f0c62c9483c949f95ece281406235c56968
Marcelo Garoze <marcelogaroze@hotmail.com>
2022-11-15 14:41:57 -0300

    refactor(express): added getTrails and fix the deletedAt problem

--- src/modules/trails/useCases/getTrails/GetTrailsUseCase.ts
+++ src/modules/trails/useCases/getTrails/GetTrailsUseCase.ts
@@ -3,6 +3,7 @@ import { prisma } from '../../../../database';
 export class GetTrailsUseCase {
   async execute() {
     return await prisma.trail.findMany({
+      where: { deletedAt: null },
       select: {
         id: true,
         title: true,
